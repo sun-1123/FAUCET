d3216a590b6be05494baff88de07053e7361525d
sun-1123 <86914008+sun-1123@users.noreply.github.com>
2021-10-16 22:20:28 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./utils/GlobalStyle";
+import Content from "./components/MyContent";
+
+function App() {
+  return (
+    <>
+      <Content />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
